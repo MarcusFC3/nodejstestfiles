fef511b9886b0a0ccefe19bd3955b3178561f64b
MarcusFC3 <22795@my4county.net>
2024-10-16 14:40:27 -0400

    finished tutorial on express js

--- expressjs/controllers/messages.controller.js
+++ expressjs/controllers/messages.controller.js
@@ -1,8 +1,12 @@
-
+const path = require("path");
 //res.send will use application/json content type for json objects
 //res.send will use content type text/html for string
 function getMessages(req, res){
-    res.send('<html><body><ul><li><p>Hello! Im in a list!</p></li></ul></body></html>')
+    res.render ("messages", {
+        title: "Views",
+        friend: "Marcus",
+    });
+    //res.send('<html><body><ul><li><p>Hello! Im in a list!</p></li></ul></body></html>')
 }
 function postMessages(req, res){
     console.log("updating message board...")
--- expressjs/server.js
+++ expressjs/server.js
@@ -1,9 +1,14 @@
 //if file is named "server.js" npm start will run the file
 const express = require("express");
-const friendsController = require("./controllers/friends.controller");
-const messagesController = require("./controllers/messages.controller");
+const path = require("path");
+
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
 
 const app = express();//creates server
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 const PORT = 3000;
 
@@ -12,27 +17,20 @@ app.use((req, res, next) => {
     const start = Date.now();
     next();
     const end = Date.now() - start;
-    console.log(`${req.method} ${req.url} ${end}ms`);
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${end}ms`);
 });
-
+//app.use('/site', express.static(path.join(__dirname, 'public')));
+app.use('/index', (req, res) =>{
+    res.render("index", {
+        title: "My Friends are Awesome!",
+        caption: "Just an Incredible View!"
+    });
+});
 app.use(express.json())
 
-const freindsRouter  = express.Router();
 
-//post requests
-app.post("/friends",friendsController.postFriend);
-//res.json ensures content will be read as json conent type
-app.get('/friends', friendsController.getFriends);
-
-
-
-
-app.get("/friends/:friendId", friendsController.getFriend);
-
-//res.send will use application/json content type for json objects
-//res.send will use content type text/html for string
-app.get("/messages", messagesController.getMessages)
-app.post("/messages", messagesController.postMessages);
-
-
+app.use('/friends',friendsRouter);//routers allow us to use them only under a specific path
+app.use("/messages",messagesRouter);
+//routers help modularization
+//why do we do this? this allows this portion of the app to be self contained
 app.listen(PORT, () => console.log("The server is listening..."));
